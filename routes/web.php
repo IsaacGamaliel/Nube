c77aea7f017a63544ae9e2f9ae3b474c9d74726e
Miguel Rojas <rojashernandezmiguel237@gmail.com>
2022-07-01 01:34:50 -0500

    Prohibir ingreso mediante rutas y controlador

--- routes/web.php
+++ routes/web.php
@@ -11,17 +11,24 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('dashboard');
 
 //Files
-Route::get('archivos/subir', 'FilesController@create')->name('file.create');
+Route::get('archivos/subir', 'FilesController@create')->name('file.create')->middleware('permission:file.create');
 
 
-Route::get('archivos/imagenes', 'FilesController@images')->name('file.images');
-Route::get('archivos/videos', 'FilesController@videos')->name('file.videos');
-Route::get('archivos/audios', 'FilesController@audios')->name('file.audios');
-Route::get('archivos/documentos', 'FilesController@documents')->name('file.documents');
+Route::get('archivos/imagenes', 'FilesController@images')->name('file.images')->middleware('permission:file.images');
 
+
+Route::get('archivos/videos', 'FilesController@videos')->name('file.videos')->middleware('permission:file.videos');
+
+
+Route::get('archivos/audios', 'FilesController@audios')->name('file.audios')->middleware('permission:file.audios');
+
+
+Route::get('archivos/documentos', 'FilesController@documents')->name('file.documents')->middleware('permission:file.documents');
 
 
 Route::post('archivos/subir', 'FilesController@store')->name('file.store');
+
+
 // Route::post('archivos/editar/{id}', 'FilesController@edit');
 Route::patch('archivos/eliminar/{id}', 'FilesController@destroy')->name('file.destroy');
 
